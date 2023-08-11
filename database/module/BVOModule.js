3c02318a19dcbd22cd4192c46d98d78bf764523d
黄泽睿 <1693142689@qq.com>
2023-08-11 10:23:10 +0800

    module文件修改

--- database/module/BVOModule.js
+++ database/module/BVOModule.js
@@ -1,9 +1,9 @@
 //导入mongoose模块
 const mongoose=require('mongoose');
 //借卖方schema对象
-let buyerSchema = new mongoose.Schema({
+let bvoSchema = new mongoose.Schema({
     user_id:{type:String,require:true,unique:true},
     bvo_id:{type:String,require:true}
 })
-let buyerModule = mongoose.model('bvo',buyerSchema)
-module.exports=buyerSchema
+let bvoModule = mongoose.model('bvo',bvoSchema)
+module.exports=bvoSchema
--- database/module/productModule.js
+++ database/module/productModule.js
@@ -8,7 +8,8 @@ let productSchema = new mongoose.Schema({
     price:{type:Number,require:true,min:0},
     category:{type:String,require:true},
     stock:{type:String,require:true,min:0},
-    image_url:{type:String,require:true}
+    image_url:{type:String,require:true},
+    valid:{type:Boolean,require:true},
 })
 let productModule = mongoose.model('product',productSchema)
 module.exports=productSchema
